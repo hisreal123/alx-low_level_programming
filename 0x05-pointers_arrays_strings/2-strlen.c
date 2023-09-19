0e390c74c59c998a561e9d3c3fcb9a625692d3a9
hisreal123 <omosgoerge@gmail.com>
2023-09-19 18:39:25 +0100

    d

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - a function returns the length of a string and checks
+ * for \0 if it exists in the code.
+ * @s: string variable
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	if (s)
+	{
+		size_t len = strlen(s);
+
+		return (len);
+	}
+
+	return (0);
+}
