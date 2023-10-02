94624a8e236531b66daa9ddf798d74c6176e3c7a
hisreal123 <omosgoerge@gmail.com>
2023-10-02 23:02:33 +0100

    d

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: input
+ * @argv: input
+ * Return: Always 0;
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
+
