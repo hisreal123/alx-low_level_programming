b0c3225fe2ba9f13b8dfeb2d795ff028fb5d85d6
hisreal123 <omosgoerge@gmail.com>
2023-10-23 14:58:18 +0100

    d

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+* pop_listint - Adds a new node at the beginning of a linked list
+* @head: A pointer to a pointer to the head of the linked list
+* @n: The integer value to be stored in the new node
+*
+* Description:
+* This function creates a new node with the given integer value 'n' and
+* adds it to the beginning of a linked list. The 'head' pointer is updated
+* to point to the new node, effectively making it the new head of the list.
+*
+* Return: A pointer to the newly added node, or NULL on failure.
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new_node;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+
+
+	if (*head != NULL)
+	{
+		new_node = *head;
+		data = (*head)->n;
+		*head = (*head)->next;
+		free(new_node);
+	}
+
+
+	return (data);
+}
