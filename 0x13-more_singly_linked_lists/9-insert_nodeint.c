b1c980b70c187fbdecead87f878b9ea06c8feb0d
hisreal123 <omosgoerge@gmail.com>
2023-10-23 21:05:33 +0100

    d

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current = *head;
+	listint_t *new_node, *current;
 	unsigned int i = 0;
 
 	if (!*head)
