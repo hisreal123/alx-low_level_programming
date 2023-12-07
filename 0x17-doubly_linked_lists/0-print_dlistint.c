3c0616079e7a394f5bfa858e739c356e5b6621dc
hisreal123 <omosgoerge@gmail.com>
2023-12-07 17:22:31 +0100

    s

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t node_count = 0;
+	size_t node_count;
+
+	node_count = 0;
+
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
-		printf("%d \n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		node_count++;
 	}
